06593bd9c47b90af9cf6a582a56cb24371b08bf3
Shuai Zhang <19383852@brookes.ac.uk>
2026-03-19 19:52:40 +0000

    updated to latest version

--- src/models.py
+++ src/models.py
@@ -2,12 +2,18 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 
 def make_lr():
-    return LogisticRegression(max_iter=500, class_weight="balanced")
+    return LogisticRegression(
+        max_iter=500,
+        class_weight="balanced",
+        verbose=1
+    )
 
 def make_rf():
     return RandomForestClassifier(
-        n_estimators=300,
+        n_estimators=100,
+        max_depth=12,
         class_weight="balanced_subsample",
         random_state=42,
-        n_jobs=-1
+        n_jobs=-1,
+        verbose=1
     )
